1a8b587755cbef2179051b86e1148d6b2b161fa3
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-24 02:14:43 -0300

    Numbers

--- numbers/num.js
+++ numbers/num.js
@@ -10,4 +10,13 @@ var n5 = Number("1.1")
 
 console.log(n5)
 
+//A propriedade Number.EPSILON representa a diferença entre 1 e o menor ponto flutuante maior que 1.
+
+const result = Math.abs(0.2 - 0.3 + 0.1);
+
+console.log(result);
+// expected output: 2.7755575615628914e-17
+
+console.log(result < Number.EPSILON);
+// expected output: true
 
