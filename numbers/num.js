0562fbd821efeeb9b928b10d7742d9b3b8e044f2
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-24 18:09:29 -0300

    numbers

--- numbers/num.js
+++ numbers/num.js
@@ -45,3 +45,21 @@ var mua = Number.isNaN(NaN);
 
 console.log(cua, mua, typeof(cua));
 
+//Toexponential()
+
+var numObj = 77.1234;
+
+console.log(numObj.toExponential());  // logs 7.71234e+1
+console.log(numObj.toExponential(4)); // logs 7.7123e+1
+console.log(numObj.toExponential(2)); // logs 7.71e+1
+console.log(77.1234.toExponential()); // logs 7.71234e+1
+console.log(77 .toExponential());     // logs 7.7e+1
+
+//ToFixed()
+
+var numObjeto = 12345.6789;
+
+numObjeto.toFixed();       // Retorna '12346': note o arredondamento, não possui nenhuma parte fracionária
+numObjeto.toFixed(1);      // Retorna '12345.7': note o arredondamento
+
+
