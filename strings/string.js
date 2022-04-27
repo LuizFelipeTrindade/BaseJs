aafa161757c9e385edb6ab75d97d12a966766bbe
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-27 17:02:30 -0300

    commit

---
+++ strings/string.js
@@ -0,0 +1,22 @@
+var str = "Essa é uma string dessejável"
+
+function retonrarString() {
+    let spar = "sou uma string";
+    return spar 
+}
+
+console.log(retonrarString())
+
+const arrow = () => {
+    return spar
+}
+
+function somar() {
+    let n1 = 10;
+    let n2 = 20;
+    return n1 + n2
+}
+
+console.log(somar());
+
+
