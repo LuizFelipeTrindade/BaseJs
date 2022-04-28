83c514357718a8120fd91462f2f1b82cc41ebb30
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-04-28 14:53:49 -0300

    boolean

---
+++ boolean/boolean.js
@@ -0,0 +1,15 @@
+//Tipos Primitivos
+
+let verdadeiro = true;
+let falso = false;
+
+//Objetos
+let verdadeiro1 = new Boolean(true)
+let falso2 = new Boolean(false)
+
+
+//Consoles
+console.log(verdadeiro, falso, verdadeiro1, falso2);
+
+console.log(typeof(verdadeiro), typeof(falso), typeof(verdadeiro1), typeof(falso2));
+
