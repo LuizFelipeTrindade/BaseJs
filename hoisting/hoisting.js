d9c3ca48b61672f331c5cecc3fd4cbf1a508c13b
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-05-01 12:18:26 -0300

    hoisting

---
+++ hoisting/hoisting.js
@@ -0,0 +1,36 @@
+function catName(name) {
+    console.log("O nome do meu gato é " + name);
+  }
+  
+  catName("Tigger");
+
+
+  /////////////////////////////////////
+
+  catNome("Chloe");
+
+  function catNome(name) {
+    console.log("O nome do meu gato é " + name);
+  }
+  /*
+  O resultado do código acima é: "O nome do meu gato é Chloe"
+  */
+
+console.log(num); // Retorna undefined
+var num;
+num = 6;
+
+
+nam = 6;
+console.log(nam); // retorna 6
+var nam;
+
+
+
+
+//Não eleva o y, já que só eleva declarações e não inicializações 
+var x = 10
+
+console.log(x, y)
+
+var y = 14;
